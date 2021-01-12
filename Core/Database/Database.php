d1db407443917c1ced7c10a18d87eeedd5ce9b82
LordAurelion <aurelien.finance04@gmail.com>
2021-01-12 10:41:52 +0100

    error page changed

---
+++ Core/Database/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Core\Database;
+
+
+use PDO;
+use PDOException;
+
+class Database extends PDO
+{
+    private static ?self $pdo = null;
+
+    private function __construct() {
+        try {
+            parent::__construct('mysql:dbname=' . DB_NAME . ';host=' . DB_HOST, DB_USER, DB_PASS, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
+            ]);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public static function getPDO(): self
+    {
+        return self::$pdo ?? new self();
+    }
+}
--- public/index.php
+++ public/index.php
@@ -9,6 +9,14 @@ Autoloader::register();
 
 define('VIEWS', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR);
 define('SCRIPTS', dirname($_SERVER['SCRIPT_NAME']) . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR);
+
+// Database params
+define('DB_NAME', 'mvc');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Debug
 define('DEBUG', false);
 
 $router = new Router($_GET['url']);
