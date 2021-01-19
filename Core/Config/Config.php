a87537dbc4055c347f8c7c060d75902bd23199cb
Minarox <mathis.sema@gmail.com>
2021-01-19 12:14:54 +0100

    test suppression ligne

--- Core/Config/Config.php
+++ Core/Config/Config.php
@@ -8,7 +8,6 @@ final class Config
 {
     public function __construct() {
         // Initialisation config file
-        $config = parse_ini_file(dirname(__DIR__,2). DIRECTORY_SEPARATOR . 'config.ini');
 
         // Public sources path
         define('VIEWS', dirname(__DIR__,2) . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR);
