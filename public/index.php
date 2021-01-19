d1bd0517f53222a190dc8c4d8a0084f241692649
LordAurelion <aurelien.finance04@gmail.com>
2021-01-19 09:03:40 +0100

    Index file problem

--- public/index.php
+++ public/index.php
@@ -16,6 +16,7 @@ $router->add('/parametrage', 'ParamController');
 $router->add('/profil', 'ProfilController');
 $router->add('/connexion', 'ConnexionController');
 $router->add('/inscription', 'InscriptionController');
+$router->add('/documentation', 'DocumentationController');
+$router->add('/recuperation', 'RecuperationController');
 
 $router->run();
-
