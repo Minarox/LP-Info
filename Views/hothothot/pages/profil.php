e2a4d72c33d5256266c19cc1a00c99992d5baec4
Minarox <mathis.sema@gmail.com>
2021-01-19 09:32:34 +0100

    Ajout image profil, aggrandisement des boites documentation et paramétrage.

--- Views/hothothot/pages/profil.php
+++ Views/hothothot/pages/profil.php
@@ -1,38 +1,44 @@
 <!-- Contenu de la page -->
 <main class="flex-container-boite decoration-top">
     <!-- Boite principale -->
-    <section class="boite">
-        <!-- Titre -->
-        <h1>Votre profil</h1>
-        <hr>
-        <!-- Formulaire -->
-        <form action="#" method="post">
-            <!-- Zones de saisie -->
-            <section class="flex-container-boite-horizontal">
-                <section class="flex-container-boite-vertical" id="flex-container-boite-vertical-left">
-                    <!-- Nom -->
-                    <label for="nom">Nom</label>
-                    <input type="text" name="nom" id="nom" placeholder="Dupont" required>
-                    <!-- Email -->
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="exemple@exemple.com" required>
+    <section class="flex-container-general stretch boite">
+        <section>
+            <img class="margin-top" id="profil-picture" src="<?= SCRIPTS . 'images' . DIRECTORY_SEPARATOR . 'profil-picture.png' ?>" alt="Image de profil du compte.">
+
+        </section>
+        <section class="margin-top separation max-width">
+            <!-- Titre -->
+            <h1 class="no-marge">Votre profil</h1>
+            <hr>
+            <!-- Formulaire -->
+            <form action="#" method="post">
+                <!-- Zones de saisie -->
+                <section class="flex-container-boite-horizontal">
+                    <section class="flex-container-boite-vertical" id="flex-container-boite-vertical-left">
+                        <!-- Nom -->
+                        <label for="nom">Nom</label>
+                        <input type="text" name="nom" id="nom" placeholder="Dupont" required>
+                        <!-- Email -->
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" placeholder="exemple@exemple.com" required>
+                    </section>
+                    <section class="flex-container-boite-vertical">
+                        <!-- Prénom -->
+                        <label for="prenom">Prénom</label>
+                        <input type="text" name="prenom" id="prenom" placeholder="Jean" required>
+                        <!-- Nouveau mot de passe -->
+                        <label for="password">Nouveau mot de passe</label>
+                        <input type="password" name="password" id="password" placeholder="***********" required>
+                    </section>
                 </section>
-                <section class="flex-container-boite-vertical">
-                    <!-- Prénom -->
-                    <label for="prenom">Prénom</label>
-                    <input type="text" name="prenom" id="prenom" placeholder="Jean" required>
-                    <!-- Nouveau mot de passe -->
-                    <label for="password">Nouveau mot de passe</label>
-                    <input type="password" name="password" id="password" placeholder="***********" required>
+                <!-- Boutons de navigation -->
+                <section class="flex-container-boite-horizontal">
+                    <button type="submit">Modifier</button>
+                    <a href="<?= ROOT ?>">Retour</a>
                 </section>
-            </section>
-            <!-- Boutons de navigation -->
-            <section class="flex-container-boite-horizontal">
-                <button type="submit">Modifier</button>
-                <a href="<?= ROOT ?>">Retour</a>
-            </section>
-        </form>
-        <a id="delete" href="#" onclick="show()">Supprimer le compte</a>
+            </form>
+            <a id="delete" href="#" onclick="show()">Supprimer le compte</a>
+        </section>
     </section>
     <article id="overlay">
         <section class="flex-container-identification decoration-top">
@@ -49,7 +55,7 @@
             </form>
         </section>
     </article>
-    <script >
+    <script>
         function show() {
             document.getElementById("overlay").style.display = "block";
         }
