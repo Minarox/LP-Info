c461e27ecb9bcae813971dd26aac5925ae5e9cbd
LordAurelion <aurelien.finance04@gmail.com>
2021-01-19 08:55:45 +0100

    Index file problem

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ use App\Core\Autoloader\Autoloader;
 use App\Core\Exceptions\RouterException;
 use App\Core\Routes\Router;
 
-file_exists(__DIR__ . '/../Core/Autoloader/Autoloader.php') ? require_once __DIR__ . '/../Core/Autoloader/Autoloader.php' : die("Can't find Autoload class file !");
-file_exists(__DIR__ . '/../config.php') ? require_once __DIR__ . '/../config.php' : die("Can't find config.php file !");
+file_exists(dirname(__DIR__) . '/Core/Autoloader/Autoloader.php') ? require_once dirname(__DIR__) . '/Core/Autoloader/Autoloader.php' : die('Autoloader class doesn\'t exists !');
+file_exists(dirname(__DIR__) . '/config.php') ? require_once dirname(__DIR__) . '/config.php' : die('config file doesn\'t exists !');
 
 Autoloader::register();
 
