a5fe5c483161ba6bc65ed8a37ecac395ae6d082b
Minarox <mathis.sema@gmail.com>
2021-03-14 18:40:50 +0100

    Added "Mentions légales" inside footer.

--- Views/base.php
+++ Views/base.php
@@ -117,7 +117,7 @@
     <section class="container">
         <p>
             &copy; <?= date("Y"); ?>
-            <a href="<?= ROOT ?>">Hothothot.fr</a>
+            <a href="<?= ROOT ?>">Hothothot.fr</a> &nbsp;|&nbsp; <a href="<?= ROOT ?>mentions-legales">Mentions légales</a>
         <p>
     </section>
 </footer>
