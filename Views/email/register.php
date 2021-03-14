1f8adab33d465a4f55612f95539d2501e9588162
aurel <aurelien.finance04@gmail.com>
2021-03-14 22:24:25 +0100

    Added email template

--- Views/email/register.php
+++ Views/email/register.php
@@ -12,12 +12,12 @@
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
     <!-- LOGO -->
     <tr>
-        <td align="center" bgcolor="#e9ecef">
+        <td align="center" bgcolor="#1E1E2E">
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                 <tr>
                     <td align="center" valign="top" style="padding: 36px 24px;">
                         <a href="https://hothothot.minarox.fr/" target="_blank" style="display: inline-block;">
-                            <img src="https://hothothot.minarox.fr/public/assets/images/logo.png" alt="Logo">
+                            <img src="./logo.png" alt="Logo">
                         </a>
                     </td>
                 </tr>
@@ -27,34 +27,33 @@
     <!-- LOGO -->
     <!-- MAIL -->
     <tr>
-        <td align="center" bgcolor="#e9ecef">
+        <td align="center" bgcolor="#1E1E2E">
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                 <tr>
-                    <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
-                        <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">
-                            Bienvenue !</h1>
+                    <td align="left" bgcolor="#27293D" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #593EC2;border-top-left-radius: 8px;border-top-right-radius: 8px">
+                        <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px; color: white"">
+                        Bienvenue !</h1>
                     </td>
                 </tr>
             </table>
         </td>
     </tr>
     <tr>
-        <td align="center" bgcolor="#e9ecef">
+        <td align="center" bgcolor="#1E1E2E">
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                 <tr>
-                    <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                        <p style="margin: 0;">Nous sommes ravis que vous vous lanciez. Tout d'abord, vous devez confirmer votre compte. Appuyez simplement sur le bouton ci-dessous.</p>
+                    <td align="left" bgcolor="#27293D" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+                        <p style="margin: 0;color: white">Nous sommes ravis que vous vous lanciez. Tout d'abord, vous devez confirmer votre compte. Appuyez simplement sur le bouton ci-dessous.</p>
                     </td>
                 </tr>
-                <!-- BOUTTON -->
                 <tr>
-                    <td align="left" bgcolor="#ffffff">
+                    <td align="left" bgcolor="#27293D">
                         <table border="0" cellpadding="0" cellspacing="0" width="100%">
                             <tr>
-                                <td align="center" bgcolor="#ffffff" style="padding: 12px;">
+                                <td align="center" bgcolor="#27293D" style="padding: 12px;">
                                     <table border="0" cellpadding="0" cellspacing="0">
                                         <tr>
-                                            <td align="center" bgcolor="#FF7B00" style="border-radius: 6px;">
+                                            <td align="center" bgcolor="#6f42c1" style="border-radius: 6px;">
                                                 <a href="<?= $uri ??= null ?>?token_email=<?= $token ??= null ?>" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">
                                                     Confirmer mon email
                                                 </a>
@@ -67,18 +66,18 @@
                     </td>
                 </tr>
                 <tr>
-                    <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
-                        <p style="margin: 0;">Cordialement,<br> L’équipe HotHotHot</p>
+                    <td align="left" bgcolor="#27293D" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom-left-radius: 8px;border-bottom-right-radius: 8px">
+                        <p style="margin: 0; color: white">Cordialement,<br> L’équipe HotHotHot</p>
                     </td>
                 </tr>
             </table>
         </td>
     </tr>
     <tr>
-        <td align="center" bgcolor="#e9ecef" style="padding: 24px;">
+        <td align="center" bgcolor="#1E1E2E" style="padding: 24px;">
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                 <tr>
-                    <td align="center" bgcolor="#e9ecef" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+                    <td align="center" bgcolor="#1E1E2E" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;color: white">
                         <p style="margin: 0;">Vous avez reçu cet e-mail, car nous avons reçu une demande de confirmation pour votre compte. Si vous n'avez pas demandé de confirmation, vous pouvez supprimer cet e-mail en toute sécurité.</p>
                     </td>
                 </tr>
@@ -91,3 +90,9 @@
 </table>
 </body>
 </html>
+
+<style>
+    body{
+        color: #1E1E2E;
+    }
+</style>
