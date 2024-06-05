fd5426a662b90b48c5f29f32dc30bfeb8a02a2fa
Minarox <contact@minarox.fr>
2024-06-05 15:15:27 +0000

    Clear Horse_sim files

--- public/assets/js/tables.js
+++
@@ -1,4 +0,0 @@
-$(window).on("load resize ", function() {
-    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-    $('.tbl-header th:last-child').css({'padding-right':scrollWidth});
-}).resize();
